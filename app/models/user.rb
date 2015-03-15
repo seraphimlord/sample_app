4830fa95d977394f4cb42068e6a0fdaab235b70d
Cedric Loy <loy_qihao_cedric@hotmail.com>
2015-03-15 22:18:57 +0800

    Finish user edit, update, index, and destory actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
     has_secure_password
 
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, allow_blank: true
 
       # Returns the hash digest of the given string.
 	def User.digest(string)
