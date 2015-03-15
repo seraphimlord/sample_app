c77dac157434f9325a84b49835303cda3961c756
Cedric Loy <loy_qihao_cedric@hotmail.com>
2015-03-15 16:04:58 +0800

    solve exercise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
 
+
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  "def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       	# Log the user in and redirect to the user's show page.
@@ -14,7 +27,7 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
-  end
+  end"
 
   # Logs in the given user.
   def log_in(user)
